795637a92b088c7048193a2d6041ab180f15917e
smruthig <smruthig01@gmail.com>
2023-12-06 17:29:57 -0800

    Write to env file

--- routes/test.js
+++ routes/test.js
@@ -1,40 +1,35 @@
 import express from 'express';
+import { promises as fsPromises } from 'fs';
 
 export const router = express.Router();
 
-const containsEscapeSequences = str => {
-  console.log('containsEscapeSequences');
-  const escapeSequences = ['\\n', '\\r', '\\t', '\\b', '\\f', '\\v', '\\0', "\\'", '\\"', '\\\\'];
-  const minLength = 6;
-  const hasUpperCase = /[A-Z]/.test(str);
-  const hasLowerCase = /[a-z]/.test(str);
-  const hasNumber = /[0-9]/.test(str);
-  const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(str);
-
-  if (str.length < minLength) {
-    return false;
-  }
-
-  for (const escapeSeq of escapeSequences) {
-    if (str.includes(escapeSeq)) {
-      return false;
-    }
-  }
-
-  return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
-};
-
 router.post('/', (req, res) => {
   console.log('req.body', req.body);
-  // const inputValue = req.body.input; // Get the input value from the request body
 
-  containsEscapeSequences('5'); // Validate the input value
+  // Your data to write to the .env file
+  const envData = `
+  USERNAME=${req.body.username}
+  PASS=${req.body.password}
+  DOMAIN=${req.body.domain}
+  `;
 
-  // if (errorMessage !== '') {
-  //   res.status(400).send({ error: errorMessage }); // Send error response with validation message
-  // } else {
-  //   res.send({ message: 'Input is valid' }); // Send success response with confirmation message
-  // }
+  // Path to your .env file
+  const envFilePath = './.env';
+
+  // Use fsPromises.writeFile for asynchronous file writing
+  fsPromises
+    .writeFile(envFilePath, envData)
+    .then(() => {
+      console.log('Data has been written to .env file');
+      return fsPromises.readFile(envFilePath, 'utf-8');
+    })
+    .then(fileContents => {
+      console.log('Contents of .env file:');
+      console.log(fileContents);
+    })
+    .catch(error => {
+      console.error('Error writing to .env file:', error);
+    });
 });
 
 router.get('/', async (req, res) => {
